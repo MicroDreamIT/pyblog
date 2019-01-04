a00880925d5198c5d9714800d3aecd9ecf3f871a
shahanursharif <sharif@microdreamit.com>
2019-01-04 18:55:01 +0600

    first

--- zone/models.py
+++ zone/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Zone(models.Model):
     name = models.CharField(max_length=100)
-    user = models.ForeignKey(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
