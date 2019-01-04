a4050b8a8f5d77d5f8b754543e437209060a1ec6
shahanursharif <sharif@microdreamit.com>
2019-01-04 17:12:42 +0600

    first

--- pyblog/urls.py
+++ pyblog/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
+    path('zone/', include('zone.urls')),
     path('admin/', admin.site.urls),
-
 ]
+
